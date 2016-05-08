1fbcf6ffc1b53a41c28c4d09b564f8cc6b537f81
Ajay Koranne <akoranne@gmail.com>
2016-05-08 08:59:12 -0500

    added debug messages

--- pipeline/gradle-build-test.sh
+++ pipeline/gradle-build-test.sh
@@ -2,14 +2,16 @@
 
 set -e # fail fast
 
-export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-8-openjdk-amd64};
-
 # The src from the 'develop' branch, injected by concourse
 srcdir=`pwd`/source-code-develop;
 
 # change to the src directory
 cd ${srcdir};
 
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-8-openjdk-amd64/jre};
+
+echo java -version;
+
 # do a clean build using gradle, and execute all tests.
 # if everything succeeds, assemble the archive (will be in the build/libs)
-./gradlew clean build test assemble;
+${JAVA_HOME:-/usr/lib/jvm/java-8-openjdk-amd64/jre} ./gradlew clean build test assemble;
