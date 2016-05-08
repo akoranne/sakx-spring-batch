ac8e1fb4bba68eb5302d744016ceef4dd49faa4a
Ajay Koranne <akoranne@gmail.com>
2016-05-08 09:02:44 -0500

    fixed echo

--- pipeline/gradle-build-test.sh
+++ pipeline/gradle-build-test.sh
@@ -10,7 +10,7 @@ cd ${srcdir};
 
 export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-8-openjdk-amd64/jre};
 
-echo java -version;
+echo "java -version";
 
 # do a clean build using gradle, and execute all tests.
 # if everything succeeds, assemble the archive (will be in the build/libs)
